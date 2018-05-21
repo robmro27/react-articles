496c80b5f64f41e703c1138958827444ba3c8ed2
robmro27 <robert.mroz1985@gmail.com>
2018-05-21 14:55:58 +0200

    move articles to separate component

--- js/components/app.body.js
+++ js/components/app.body.js
@@ -10,16 +10,10 @@ class AppBody extends React.Component
     render() {
         return (
             <main role="main" className="mainContainer container">
-                <h1 className="mt-5">Articles</h1>
 
-                <div>
-                    <Article title="Title" subtitle="subtitle..." content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."/>
-                    <Article title="Title" subtitle="subtitle..." content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."/>
-                </div>
-                <div className="clearfix"></div>
+                <Articles />
 
                 <a href="#" onClick={this.onToggleShowAdd.bind(this)}>{(this.state.showAdd) ? 'Hide' : 'Show'} form</a>
-
                 {this.state.showAdd && <ArticleNew />}
 
             </main>
